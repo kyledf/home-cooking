2df19d1f3ed6ee75d841554dfe173ad4c6b5dbf3
kyleCS50 <kyledanielfrancis@gmail.com>
2022-10-12 15:42:32 +1300

    Added earch bar to feed

--- components/feed/FeedScreen.js
+++ components/feed/FeedScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  SafeAreaView,
   Text,
   StyleSheet,
   View,
@@ -10,6 +9,8 @@ import {
 } from "react-native";
 import { lightTheme } from "../../themes/themes";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { Searchbar } from "react-native-paper";
+import { useEffect } from "react";
 
 const currencyFormat = (num) => {
   return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
@@ -49,6 +50,10 @@ const mealInfo = [
 ];
 
 const FeedScreen = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = React.useState("Chicken and Rice");
+
+  useEffect(() => {}, [searchQuery]);
+
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
@@ -58,41 +63,50 @@ const FeedScreen = ({ navigation }) => {
         >
           <Ionicons name="menu" size={45} color={lightTheme.background} />
         </TouchableOpacity>
+        <Searchbar
+          placeholder="Search Meal"
+          onChangeText={(query) => setSearchQuery(query)}
+          value={searchQuery}
+          style={styles.searchBar}
+        />
       </View>
       <ScrollView
         style={styles.scrollContainer}
         contentContainerStyle={{ alignItems: "center" }}
       >
-        {mealInfo.map((meal) => (
-          <View style={styles.mealContainer} key={meal.id}>
-            <Image
-              style={styles.mealImage}
-              source={{ uri: meal.image }}
-            ></Image>
-            <View style={styles.mealInfo}>
-              <Text style={styles.mealName}>{meal.name}</Text>
-              <Text style={styles.mealPrice}>
-                Price per serving:{" "}
-                {currencyFormat(meal.totalPrice / meal.quantity)}
-              </Text>
-              <Text style={styles.mealLocation}>
-                Pick Up Location: {meal.location}
-              </Text>
-            </View>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity style={[styles.button, styles.messageButton]}>
-                <Text style={[styles.buttonText, styles.messageButtonText]}>
-                  Message Seller
+        {mealInfo
+          .map(({ id, image, name, totalPrice, quantity, location }) => (
+            <View style={styles.mealContainer} key={id}>
+              <Image
+                style={styles.mealImage}
+                source={{ uri: image }}
+              ></Image>
+              <View style={styles.mealInfo}>
+                <Text style={styles.mealName}>{name}</Text>
+                <Text style={styles.mealPrice}>
+                  Price per serving:
+                  {currencyFormat(totalPrice / quantity)}
                 </Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.button, styles.moreInfoButton]}>
-                <Text style={[styles.buttonText, styles.moreInfoButtonText]}>
-                  See More Info
+                <Text style={styles.mealLocation}>
+                  Pick Up Location: {location}
                 </Text>
-              </TouchableOpacity>
+              </View>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity style={[styles.button, styles.messageButton]}>
+                  <Text style={[styles.buttonText, styles.messageButtonText]}>
+                    Message Seller
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.button, styles.moreInfoButton]}
+                >
+                  <Text style={[styles.buttonText, styles.moreInfoButtonText]}>
+                    See More Info
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        ))}
+          ))}
       </ScrollView>
     </View>
   );
@@ -118,6 +132,15 @@ const styles = StyleSheet.create({
   menuButton: {
     position: "absolute",
     left: "5%",
+  },
+  searchBar: {
+    position: "absolute",
+    left: "20%",
+    width: "75%",
+    height: "30%",
+    borderRadius: 25,
+    backgroundColor: lightTheme.background,
+    marginTop: 50,
   },
   scrollContainer: {
     width: "100%",
