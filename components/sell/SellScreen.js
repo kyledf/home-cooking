ae32beb0b374c4c82503586e6fd01d7793f3cb2c
kyleCS50 <kyledanielfrancis@gmail.com>
2022-10-12 15:05:01 +1300

    Created Sell Screen

--- components/sell/SellScreen.js
+++ components/sell/SellScreen.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   View,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { lightTheme } from "../../themes/themes";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -19,8 +20,22 @@ const SellScreen = ({ navigation }) => {
         >
           <Ionicons name="menu" size={45} color={lightTheme.background} />
         </TouchableOpacity>
+        <Text style={styles.title}>Sell a Meal</Text>
       </View>
-      <Text>Sell Screen</Text>
+      <TouchableOpacity style={styles.imageButton}>
+        <Ionicons name="images" size={"50%"} color={lightTheme.maroon} />
+        <Text style={styles.buttonText}>Add Image</Text>
+      </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Title"/>
+        <TextInput style={styles.input} placeholder="Description" />
+        <TextInput style={styles.input} placeholder="Quantity"/>
+        <TextInput style={styles.input} placeholder="Price"/>
+        <TextInput style={styles.input} placeholder="Location"/>
+      </View>
+      <TouchableOpacity style={styles.postButton}>
+        <Text style={styles.postButtonText}>Post Your Meal</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -46,6 +61,67 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: "5%",
   },
+  title: {
+    position: "absolute",
+    left: "25%",
+    top: "40%",
+    textAlign: "center",
+    color: lightTheme.background,
+    fontSize: 40,
+    fontWeight: "900",
+  },
+  imageButton: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    position: "absolute",
+    top: "20%",
+    height: "20%",
+    width: "80%",
+    padding: "10%",
+    flexDirection: "row",
+    backgroundColor: lightTheme.lightPurple,
+    borderColor: lightTheme.maroon,
+    borderWidth: 4,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: lightTheme.background,
+  },
+  inputContainer: {
+    marginTop: "70%",
+    width: "80%",
+    height: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "100%",
+    height: "20%",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: lightTheme.lightPurple,
+    marginTop: "5%",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  postButton: {
+    marginTop: "15%",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: lightTheme.maroon,
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  postButtonText: {
+    fontSize: 18,
+    color: lightTheme.background,
+    fontWeight: "bold",
+  },
+
 });
 
 export default SellScreen;
