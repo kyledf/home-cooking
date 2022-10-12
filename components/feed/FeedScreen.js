6af10651b2b89f2b613641ca01d58cc5e82b7673
kyleCS50 <kyledanielfrancis@gmail.com>
2022-10-12 18:10:31 +1300

    Added earch bar functionality

--- components/feed/FeedScreen.js
+++ components/feed/FeedScreen.js
@@ -1,20 +1,10 @@
 import React from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  ScrollView,
-  Image,
-} from "react-native";
+import { StyleSheet, View, TouchableOpacity, ScrollView } from "react-native";
 import { lightTheme } from "../../themes/themes";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { Searchbar } from "react-native-paper";
-import { useEffect } from "react";
-
-const currencyFormat = (num) => {
-  return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-};
+import { useEffect, useState } from "react";
+import MealCard from "./MealCard";
 
 const mealInfo = [
   {
@@ -50,9 +40,13 @@ const mealInfo = [
 ];
 
 const FeedScreen = ({ navigation }) => {
-  const [searchQuery, setSearchQuery] = React.useState("Chicken and Rice");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {}, [searchQuery]);
+  const filterMeals = () => {
+   return mealInfo.filter((meal) => {
+       return meal.name.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -74,39 +68,18 @@ const FeedScreen = ({ navigation }) => {
         style={styles.scrollContainer}
         contentContainerStyle={{ alignItems: "center" }}
       >
-        {mealInfo
-          .map(({ id, image, name, totalPrice, quantity, location }) => (
-            <View style={styles.mealContainer} key={id}>
-              <Image
-                style={styles.mealImage}
-                source={{ uri: image }}
-              ></Image>
-              <View style={styles.mealInfo}>
-                <Text style={styles.mealName}>{name}</Text>
-                <Text style={styles.mealPrice}>
-                  Price per serving:
-                  {currencyFormat(totalPrice / quantity)}
-                </Text>
-                <Text style={styles.mealLocation}>
-                  Pick Up Location: {location}
-                </Text>
-              </View>
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity style={[styles.button, styles.messageButton]}>
-                  <Text style={[styles.buttonText, styles.messageButtonText]}>
-                    Message Seller
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={[styles.button, styles.moreInfoButton]}
-                >
-                  <Text style={[styles.buttonText, styles.moreInfoButtonText]}>
-                    See More Info
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          ))}
+        {filterMeals().map(
+          ({ id, image, name, totalPrice, quantity, location }) => (
+            <MealCard
+              key={id}
+              image={image}
+              name={name}
+              totalPrice={totalPrice}
+              quantity={quantity}
+              location={location}
+            />
+          )
+        )}
       </ScrollView>
     </View>
   );
@@ -146,75 +119,6 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     marginTop: 150,
-  },
-  mealContainer: {
-    width: "95%",
-    height: 400,
-    backgroundColor: lightTheme.darkPurple,
-    marginTop: "5%",
-    marginBottom: "5%",
-    borderRadius: 10,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  mealImage: {
-    width: "90%",
-    height: "60%",
-    borderRadius: 10,
-  },
-  mealInfo: {
-    width: "90%",
-    marginTop: "2%",
-  },
-  mealName: {
-    fontSize: 25,
-    fontWeight: "bold",
-    color: lightTheme.background,
-  },
-  mealLocation: {
-    fontWeight: "bold",
-    fontSize: 15,
-    color: lightTheme.background,
-  },
-  mealPrice: {
-    fontWeight: "bold",
-    fontSize: 15,
-    color: lightTheme.background,
-  },
-  buttonContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "90%",
-    marginTop: "2%",
-  },
-  button: {
-    backgroundColor: lightTheme.background,
-    borderRadius: 10,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: "2%",
-    paddingVertical: "4%",
-    width: "45%",
-  },
-  buttonText: {
-    fontSize: 15,
-    fontWeight: "bold",
-  },
-  messageButton: {
-    backgroundColor: lightTheme.maroon,
-  },
-  moreInfoButton: {
-    backgroundColor: lightTheme.background,
-  },
-  messageButtonText: {
-    color: lightTheme.background,
-  },
-  moreInfoButtonText: {
-    color: lightTheme.maroon,
   },
 });
 
---
+++ components/feed/MealCard.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { Text, StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import { lightTheme } from "../../themes/themes";
+
+const currencyFormat = (num) => {
+  return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+};
+
+const MealCard = ({image, name, totalPrice, quantity, location}) => {
+  return (
+    <View style={styles.mealContainer}>
+      <Image style={styles.mealImage} source={{ uri: image }}></Image>
+      <View style={styles.mealInfo}>
+        <Text style={styles.mealName}>{name}</Text>
+        <Text style={styles.mealPrice}>
+          Price per serving:
+          {currencyFormat(totalPrice / quantity)}
+        </Text>
+        <Text style={styles.mealLocation}>Pick Up Location: {location}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, styles.messageButton]}>
+          <Text style={[styles.buttonText, styles.messageButtonText]}>
+            Message Seller
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.moreInfoButton]}>
+          <Text style={[styles.buttonText, styles.moreInfoButtonText]}>
+            See More Info
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealContainer: {
+    width: "95%",
+    height: 400,
+    backgroundColor: lightTheme.darkPurple,
+    marginTop: "5%",
+    marginBottom: "5%",
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mealImage: {
+    width: "90%",
+    height: "60%",
+    borderRadius: 10,
+  },
+  mealInfo: {
+    width: "90%",
+    marginTop: "2%",
+  },
+  mealName: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: lightTheme.background,
+  },
+  mealLocation: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: lightTheme.background,
+  },
+  mealPrice: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: lightTheme.background,
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+    marginTop: "2%",
+  },
+  button: {
+    backgroundColor: lightTheme.background,
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: "2%",
+    paddingVertical: "4%",
+    width: "45%",
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  messageButton: {
+    backgroundColor: lightTheme.maroon,
+  },
+  moreInfoButton: {
+    backgroundColor: lightTheme.background,
+  },
+  messageButtonText: {
+    color: lightTheme.background,
+  },
+  moreInfoButtonText: {
+    color: lightTheme.maroon,
+  },
+});
+
+export default MealCard;
